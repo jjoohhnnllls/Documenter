cbc1be450869888010156a9a7a208437226717c8
Your Name <your.email@example.com>
2024-11-22 11:30:48 +0800

    22/11/2024

--- recorder.py
+++ recorder.py
@@ -1,18 +1,68 @@
 import cv2
 import numpy as np
-import pyautogui
-from datetime import datetime
+import pyautogui as pyag
+import mss
+import os
+import time
 
 
 
 def start_recording():
-    #specify solution
-    resolution = (1920,1080)
-    #video codec
-    codec = cv2.VideoWriter_fourcc(*"XVID")
+    ##take screenshots or frames
+    #screenshot= pyag.screenshot()
+    ##save screenshot to captures folder
+    #save_path = os.path.join("captures", "screenshot.png")
+    #screenshot.save(save_path)
+    ##loop continuos screenshot until user stop says stop
+
+
+    # Create or ensure 'captures' folder exists
+    if not os.path.exists("captures"):
+        os.makedirs("captures")
+
+    try:
+        count = 1
+        #select display
+        with mss.mss() as sct:
+            #list available displays
+            monitors = sct.monitors
+            print("Available Displays: ")
+            for i,monitor in enumerate(monitors):
+                print(f"{i}: Monitor {i}. {monitor}")
+
+
+            #select which monitor to use
+            selected_monitor_input = input("\nEnter number of displays")
+            
+
+
+
+
+
+
+        while True:
+            #take screenshots or frames
+            screenshot = pyag.screenshot()
+            #save screenshot to captures folder
+            save_path = os.path.join("captures", f"SS_{count}.png")
+            screenshot.save(save_path)
+
+            print(f"Screenshot {count} taken and saved to folder")
+            count += 1 
+
+            #wait for 1 second (time interval )
+            time.sleep(1)
+         
+    except KeyboardInterrupt:
+        print("\nscreenshot capture stopped by user.")
+
+    
 
 def end_recording():
     ...
 
 def extract_frames():
     ...
+
+
+start_recording()
